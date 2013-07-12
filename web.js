f74971a3c6b25e43d9e1a4c1d86c39eadd443571
Xaquin Vazquez <xaquinvp@gmail.com>
2013-07-12 12:03:26 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  var txtIndex = fs.readFileSync('./index.html');
+  response.send(txtIndex.toString());
+//  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
